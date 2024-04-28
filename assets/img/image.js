c935a8bc25a91bf2406701833ffeb4d0daac0d12
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2024-04-28 23:56:44 +0500

    Fonts and Images

---
+++ assets/img/image.js
@@ -0,0 +1,8 @@
+const IMAGES = {
+    LOGO: require('./porscheL.png'),
+    LOGIN: require('./LoginCar.png'),
+    CAR2: require('./car2.png'),
+    LOADING: require('./loading.jpg'),
+};
+
+export default IMAGES;
